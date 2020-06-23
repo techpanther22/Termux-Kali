53653d5940c12f8a580a9d5a81af71a8e9cb866f
techpanther22 <piiiiyush22@gmail.com>
2020-06-23 21:31:40 +0530

    Update installkali.sh

--- installkali.sh
+++ installkali.sh
@@ -4,10 +4,8 @@ printf " \e[32;1m[+] Updating packages ... \e[0m\n"
 apt update -y && apt upgrade -y
 printf " \e[32;1m[+] Installing kali... \e[0m\n"
 pkg install wget openssl-tool proot -y && hash -r && wget https://raw.githubusercontent.com/EXALAB/AnLinux-Resources/master/Scripts/Installer/Kali/kali.sh && bash kali.sh
-mv kali.sh ../kali.sh
-/data/data/com.termux/files/home/kali.sh
-cd ..
-rm -rf /data/data/com.termux/files/home/Termux-Kali
+mv kali-fs start-kali.sh kali-binds kali.sh ../ -r
+printf "\e[33;1m        [+]  Run '\e[32;1mcd ..\e[33;1m' command \e[0m\n"
 printf "\e[33;1m        [+]  Run '\e[32;1m./start.sh\e[33;1m' command to start Kali  \e[0m\n"
 printf "\e[0m\n"
 printf "\e[0m\n"
@@ -16,5 +14,5 @@ printf "\e[32;1m     [+]  Youtube  :  youtube.com/techpanther     [+]  \e[0m\n"
 printf "\e[32;1m     [+]  Instagram:  instagram.com/techpanther   [+]  \e[0m\n"
 printf "\e[32;1m     [+]  Facebook :  facebook.com/techpanther22  [+]  \e[0m\n"
 printf "\e[32;1m     [+]  Blog     :  techpanther.in              [+]  \e[0m\n"
-printf "\e[32;1m     [+]  github   :  github.com/techpanther22[+]  \e[0m\n"
+printf "\e[32;1m     [+]  github   :  github.com/techpanther22    [+]  \e[0m\n"
 exit
