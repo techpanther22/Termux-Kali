4af932005f21f566730ed593254b3ff707c47846
techpanther22 <piiiiyush22@gmail.com>
2020-06-23 20:24:37 +0530

    Update kali.sh

--- kali.sh
+++ kali.sh
@@ -14,3 +14,4 @@ printf "\e[32;1m     [+]  Instagram:  instagram.com/techpanther   [+]  \e[0m\n"
 printf "\e[32;1m     [+]  Facebook :  facebook.com/techpanther22  [+]  \e[0m\n"
 printf "\e[32;1m     [+]  Blog     :  techpanther.in              [+]  \e[0m\n"
 printf "\e[32;1m     [+]  github   :  github.com/techpanther22[+]  \e[0m\n"
+exit
